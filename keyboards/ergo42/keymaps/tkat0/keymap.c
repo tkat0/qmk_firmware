b28a18b384658b6fc1bb7efdb83955b0416a129b
tkat0 <tkato.dev@gmail.com>
2019-12-28 16:20:14 +0900

    update

--- keyboards/ergo42/keymaps/tkat0/keymap.c
+++ keyboards/ergo42/keymaps/tkat0/keymap.c
@@ -4,6 +4,7 @@
 
 extern keymap_config_t keymap_config;
 
+// Layer defenition
 #define BASE 0
 #define META 1
 #define SYMB 2
@@ -32,23 +33,23 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* BASE
- * ,------------------------------------------------.   ,------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   |      |   Y  |   U  |   I  |   O  |   P  |  \   |
- * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |      |   |      |   H  |   J  |   K  |   L  |   ;  |  -   |
- * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
- * | Sft  |   Z  |   X  |   C  |   V  |   B  |      |   |      |   N  |   M  |   ,  |   .  |   /  |  =   |
- * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * |      |      |      | ALt  | Esc/ |Enter/| Lang |   | Bksp |Space/|SYMB  |      |      |      |      |
- * |      |      |      |      | GUI  |Shift |      |   |      |Meta  |      |      |      |      |      |
- * `------------------------------------------------'   `------------------------------------------------'
- */
-[BASE] = LAYOUT( \
-    KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,             KC_T,          KC_RBRC,       KC_BSLS,    KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS, \
-    KC_LCTRL, KC_A,    KC_S,   KC_D,    KC_F,             KC_G,          S(KC_8),       S(KC_9),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_MINUS, \
-    KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,             KC_B,          S(KC_RBRC),    S(KC_BSLS), KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_EQL, \
-    XXXXXXX,      XXXXXXX,  XXXXXXX,KC_LALT, MY_ESC_G, MY_ENT_S,   MY_LANG,  KC_BSPC,  MY_SPC_META,   MO(SYMB),  XXXXXXX,XXXXXXX,  XXXXXXX,XXXXXXX   \
+  /* BASE
+   * ,------------------------------------------------.   ,------------------------------------------------.
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   |      |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |      |   |      |   H  |   J  |   K  |   L  |   ;  |  -   |
+   * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
+   * | Sft  |   Z  |   X  |   C  |   V  |   B  |      |   |      |   N  |   M  |   ,  |   .  |   /  |  =   |
+   * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
+   * |      |      |      | ALt  | Esc/ |Enter/| Lang |   | Bksp |Space/|SYMB  |      |      |MY_PS1|MY_PS2|
+   * |      |      |      |      | GUI  |Shift |      |   |      |Meta  |      |      |      |      |      |
+   * `------------------------------------------------'   `------------------------------------------------'
+   */
+  [BASE] = LAYOUT( \
+      KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,     XXXXXXX,    XXXXXXX, KC_Y,        KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS, \
+      KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,     XXXXXXX,    XXXXXXX, KC_H,        KC_J,     KC_K,    KC_L,    KC_SCLN, KC_MINUS, \
+      KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,     XXXXXXX,    XXXXXXX, KC_N,        KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_EQL, \
+      XXXXXXX,  XXXXXXX, XXXXXXX, KC_LALT, MY_ESC_G, MY_ENT_S, MY_LANG,    KC_BSPC, MY_SPC_META, MO(SYMB), XXXXXXX, XXXXXXX, MY_PS1,  MY_PS2   \
   ),
 
 
@@ -64,10 +65,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [META] = LAYOUT( \
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,    _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-    _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______,    _______,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
-    _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______,    _______,    KC_QUOT, KC_GRV,  _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, _______, _______, _______ \
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+    _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______,    _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
+    _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______,    _______, KC_QUOT, KC_GRV,  _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______ \
   ),
 
   /* SYMB
@@ -82,10 +83,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
-    _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, _______, _______, _______, \
-    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,    _______,    KC_CIRC, KC_AMPR, KC_ASTR, KC_F11,  KC_F12,  _______, \
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,    _______,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
-    _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, _______, _______, _______ \
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,    _______,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,    _______,  KC_CIRC, KC_AMPR, KC_ASTR, KC_F11,  KC_F12,  _______, \
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,    _______,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
+    _______, _______, _______, _______, _______, _______, _______,    _______,  _______, _______, _______, _______, _______, _______ \
   )
 
 
@@ -94,29 +95,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 static bool my_esc_g_pressed = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//  static uint16_t my_hash_timer;
 
     switch (keycode) {
         // Tap: Esc, Long Tap: GUI
         case MY_ESC_G:
             if (record->event.pressed) {
-                my_esc_g_pressed = true;
                 // on keydown
-//        my_hash_timer = timer_read();
-//        register_code(KC_LGUI);
+                my_esc_g_pressed = true;
             } else {
                 // on keyup
                 my_esc_g_pressed = false;
                 unregister_code(KC_LGUI);
-//        if (timer_elapsed(my_hash_timer) < TAPPING_TERM) {
-//          SEND_STRING(SS_TAP(X_ESCAPE));
-//        }
                 register_code(KC_ESC);
                 unregister_code(KC_ESC);
             }
             return false;
         default:
             if (my_esc_g_pressed) {
+                // MY_ESC_Gが押された状態で他のキーが押されたときにLGUIを発行する
                 register_code(KC_LGUI);
             }
             break;
